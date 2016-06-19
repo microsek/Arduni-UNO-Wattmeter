7b994f6bd357b87ef52e44d0100ac53b8719a1de
Seksan <microsekmcu@gmail.com>
2016-06-19 20:10:41 +0700

    add lib emon and l2c lcd
    add lib emon and l2c lcd

--- meter.ino
+++ meter.ino
@@ -10,6 +10,7 @@ LiquidCrystal_I2C lcd(0x3f, 20, 4);
 int voltageValue = 0; 
 float unitValue=0;
 float totalunitValue=0;
+float power=0;
 
 void setup()
 {
@@ -17,27 +18,30 @@ void setup()
   lcd.init();
   // Turn on the blacklight and print a message.
   lcd.backlight();
-  emon1.voltage(2, 260.26, 2.5);  // Voltage: input pin, calibration, phase_shift
-  emon1.current(1, 6);       // Current: input pin, calibration.
+  emon1.voltage(2, 260.26, 0);  // Voltage: input pin, calibration, phase_shift
+  emon1.current(1, 6.5);       // Current: input pin, calibration.
+  delay(2000);
 }
 
 void loop()
 {
-  emon1.calcVI(20,2000);         // Calculate all. No.of half wavelengths (crossings), time-out
-  float realPower       = emon1.realPower;        //extract Real Power into variable
-  float apparentPower   = emon1.apparentPower;    //extract Apparent Power into variable
-  float powerFActor     = emon1.powerFactor;      //extract Power Factor into Variable
-  float supplyVoltage   = emon1.Vrms;             //extract Vrms into Variable
-  float Irms            = emon1.Irms;             //extract Irms into Variable
-  unitValue=apparentPower*0.000278;
-  totalunitValue=totalunitValue+unitValue;
+  
+  emon1.calcVI(30,2000);         // Calculate all. No.of half wavelengths (crossings), time-out
+  float voltage   = emon1.Vrms;             //extract Vrms into Variable
+  float current   = emon1.Irms;             //extract Irms into Variable
+  if (current>0.07)
+  {
+    power=(voltage*current)/1000;
+    unitValue=power*0.000278;
+    totalunitValue+=unitValue;
+  }
   lcd.setCursor(0, 0);
   lcd.print("Voltage= ");
-  lcd.print(supplyVoltage);
+  lcd.print(voltage);
   lcd.print(" Vac  ");
   lcd.setCursor(0, 1);
   lcd.print("Current= ");
-  lcd.print(Irms);
+  lcd.print(current);
   lcd.print(" A(ac)  ");
   lcd.setCursor(0, 2);
   lcd.print("Unit = ");
@@ -46,6 +50,7 @@ void loop()
   lcd.setCursor(0, 3);
   lcd.print("Total unit= ");
   lcd.print(totalunitValue);
+  unitValue=0;
   
  delay(1000); // wait 10 milliseconds before the next loop 	// Do nothing here...
 }
